1efd82837009d952ffd36ae8ee136fc4d08322a4
Maksym Voievodskyi <maksimvoevodskij81@gmail.com>
2023-01-27 17:26:29 +0100

    fix name templete

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -25,7 +25,7 @@ namespace StarChart.Controllers
             celestialObject.Satellites = _context.CelestialObjects.Where(x => x.OrbitedObjectId == id).ToList();   
             return Ok(celestialObject);
         }
-        [HttpGet("name")]
+        [HttpGet("{name}")]
         public IActionResult GetByName(string name)
         {
             var celestialObject = _context.CelestialObjects.FirstOrDefault(x => x.Name == name);
