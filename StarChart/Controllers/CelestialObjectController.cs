7ace841c0bdce530da7395b6eff823a7e2c8324f
Maksym Voievodskyi <maksimvoevodskij81@gmail.com>
2023-01-27 16:48:51 +0100

    add get methods to CelestialObjectController

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -12,6 +15,28 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id) 
+        {
+            var celestialObject = _context.CelestialObjects.FirstOrDefault(x => x.Id == id);
+            if(celestialObject is null) 
+                return NotFound();
+            return Ok(celestialObject);
+        }
+        [HttpGet("name")]
+        public IActionResult GetByName(string name)
+        {
+            var celestialObject = _context.CelestialObjects.FirstOrDefault(x => x.Name == name);
+            if (celestialObject is null)
+                return NotFound();
+            return Ok(celestialObject);
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+           return Ok(_context.CelestialObjects);
+
         }
     }
 }
